43940f8d3f75fdc1c1617d6577350170abe5cc6f
Allen Liu <alliucaneat@gmail.com>
2020-10-12 17:59:23 -0400

    initial v1 head builder

---
+++ yolo/modeling/model_heads/_Yolov1Head.py
@@ -0,0 +1,116 @@
+import tensorflow as tf
+import tensorflow.keras as ks
+
+import importlib
+import collections
+from typing import *
+
+import yolo.modeling.building_blocks as nn_blocks
+from . import configs
+
+class Yolov1Head(tf.keras.Model):
+    def __init__(self, 
+                 model="regular", 
+                 classes=20,
+                 S=7,
+                 boxes=2, 
+                 config=None, 
+                 input_shape=(None, None, None, 3), 
+                 **kwargs):
+        """
+        Args:
+            model: to generate a standard yolo head, we have string key words that can accomplish this
+                    regular -> corresponds to yolov1
+            S: integer for the number of grid cells in SxS output
+            classes: integer for the number of classes in the prediction
+            boxes: integer for the total number of bounding boxes
+            config: 
+
+        """
+
+        self._config = config
+        self._classes = classes
+        self._S = S
+        self._boxes = boxes
+        self._model_name = model
+
+        if not isinstance(config, Dict):
+            self._config = self.load_cfg(model) 
+        else:
+            self._model_name = "custom_head"
+
+        self._input_shape = input_shape
+
+        inputs = ks.layers.Input(shape=self._input_shape[1:])
+        outputs = self._connect_layers(self, self._config, inputs)
+        super().__init__(inputs=inputs, 
+                         outputs=outputs, 
+                         name=self._model_name, 
+                         **kwargs)
+        return
+
+    @classmethod
+    def load_cfg(self, model):
+        """find the config file and load it for use"""
+        try:
+            head = importlib.import_module('yolov1_'+ model,
+                                            package=configs.__package__).head
+        except ModuleNotFoundError as e:
+        if e.name == configs.__package__ + '.yolov1_' + model:
+            raise ValueError(f"Invlid head '{model}'") from e
+        else:
+            raise 
+        return head_build_block_specs(head)
+
+    def head_build_block_specs(config):
+        specs = []
+        for layer in config:
+            specs.append(HeadBlockConfig(*layer))
+        return specs
+    
+    def _connect_layers(self, layers, inputs):
+        x = inputs
+        for i, layer in enumerate(layers):
+            if layer.name == "DarkConv":
+                x = nn_blocks.DarkConv(filters=layer.filters, 
+                                        kernel_size=layer.kernel_size,
+                                        strides=layer.strides,
+                                        padding=layer.padding, 
+                                        activation=layer.activation)(x)
+            elif layer.name == "Local":
+                x = ks.layers.LocallyConnected2D(filters=layer.filters, 
+                                                 kernel_size=layer.kernel_size, 
+                                                 strides=layer.strides,
+                                                 padding=layer.padding,
+                                                 activation=layer.activation)(x)
+            elif layer.name == "Dropout":
+                x = ks.layers.Dropout(rate=layer.filters)(x)
+            elif layer.name == "Connected":
+                x = ks.layers.Dense(units=layer.filters, 
+                                    activation=layer.activation)(x)
+        return x
+
+class HeadBlockConfig(object):
+    def __init__(self, layer, filters, kernel_size,
+                 strides, padding, activation):
+        '''
+        get layer config to make code more readable
+
+        Args:
+            layer: string layer name
+            filters: integer for the filter for this layer, or the output depth
+            kernel_size: integer or none, if none, it implies that the the building 
+                         block handles this automatically. not a layer input
+            downsample: boolean, to down sample the input width and height
+        '''
+        self.name = layer
+        self.filters = filters
+        self.kernel_size = kernel_size
+        self.strides = strides
+        self.padding = padding
+        self.activation = activation
+        return
+
+    def __repr__(self):
+        return f"layer: {self.name}, stack: {self.stack}, repititions: {self.repetitions}, filters: {self.filters}, bottleneck: {self.bottleneck}, route: {self.route}, output: {self.output_name}\n"
+
---
+++ yolo/modeling/model_heads/configs/yolov1_regular.py
@@ -0,0 +1,9 @@
+# (name, filters, kernal_size, strides, padding, activation)
+backbone = [
+    ("DarkConv", 1024, 3, 1, "valid", "leaky"),
+    ("DarkConv", 1024, 3, 2, "valid", "leaky"),
+    ("DarkConv", 1024, 3, 1, "valid", "leaky"),
+    ("DarkConv", 1024, 3, 1, "valid", "leaky"),
+    ("Local", 256, 3, 1, "valid", "leaky"),
+    ("Connected", 1715, None, None, None, "linear")
+]
