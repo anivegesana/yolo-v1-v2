3dba739b5b8494bd9c5ded86c1f91713eab81b70
hojoungjang <jang88@purdue.edu>
2020-10-29 00:30:49 +0900

    Designate output layer name for hidden layers (for unit testing)

--- yolo/modeling/backbones/configs/yolov1_backbone.py
+++ yolo/modeling/backbones/configs/yolov1_backbone.py
@@ -3,18 +3,18 @@
 """
 backbone = [
     ("DarkConv", 1, 64, 7, 2, "same", False, False, None, False),
-    ("MaxPool", 1, None, 2, 2, "valid", False, False, None, False),
+    ("MaxPool", 1, None, 2, 2, "valid", False, True, "64", False),
 
     ("DarkConv", 1, 192, 3, 1, "same", False, False, None, False),
-    ("MaxPool", 1, None, 2, 2, "valid", False, False, None, False),
+    ("MaxPool", 1, None, 2, 2, "valid", False, True, "192", False),
 
     ("DarkRouteProcess", 1, 256, None, None, None, False, False, None, False),
     ("DarkRouteProcess", 1, 512, None, None, None, False, False, None, False),
-    ("MaxPool", 1, None, 2, 2, "valid", False, False, None, False),
+    ("MaxPool", 1, None, 2, 2, "valid", False, True, "512", False),
 
     ("DarkRouteProcess", 4, 512, None, None, None, False, False, None, False),
     ("DarkRouteProcess", 1, 1024, None, None, None, False, False, None, False),
-    ("MaxPool", 1, None, 2, 2, "valid", False, False, None, False),
+    ("MaxPool", 1, None, 2, 2, "valid", False, True, "1024_1", False),
 
-    ("DarkRouteProcess", 2, 1024, None, None, None, False, True, None, False),
+    ("DarkRouteProcess", 2, 1024, None, None, None, False, True, "1024_2", False),
 ]
