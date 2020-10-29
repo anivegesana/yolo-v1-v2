28a62d1f024eeef34851f98b337254ae3d136346
hojoungjang <jang88@purdue.edu>
2020-10-29 10:39:54 +0900

    Fix typo in _DarkConv.py

--- yolo/modeling/building_blocks/_DarkConv.py
+++ yolo/modeling/building_blocks/_DarkConv.py
@@ -97,7 +97,6 @@ class DarkConv(ks.layers.Layer):
         kernel_size = self._kernel_size if type(
             self._kernel_size) == int else self._kernel_size[0]
         if self._padding == "same" and kernel_size != 1:
-            self._zeropad = ks.layers.ZeroPadding2D(
             padding = kernel_size - 1
             self._zeropad = ks.layers.ZeroPadding2D(((padding//2, padding//2), (padding//2, padding//2))) 
         else:
