6b36382b1abf5bb24704552eb0590368456dba12
anivegesana <anirudh.vegesana@gmail.com>
2020-11-02 14:11:18 -0500

    Pad YOLOv1 local layer in head

--- yolo/modeling/model_heads/configs/yolov1_regular.py
+++ yolo/modeling/model_heads/configs/yolov1_regular.py
@@ -4,6 +4,6 @@ head = [
     ("DarkConv", 1024, 3, 2, "same", "leaky"),
     ("DarkConv", 1024, 3, 1, "same", "leaky"),
     ("DarkConv", 1024, 3, 1, "same", "leaky"),
-    ("Local", 256, 3, 1, "valid", "leaky"),
+    ("Local", 256, 3, 1, "same", "leaky"),
     ("Connected", 1715, None, None, None, "linear")
 ]
